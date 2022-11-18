667bdb8bbad2697c5a473123b40fb040fa51e74c
Neal Sison <nealsison22@gmail.com>
2022-11-17 21:51:25 -0800

    ALL JAVASCRIPT DONE I THINK

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // ALL JS CODE TAKEN/ADAPTED FROM
 // - https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript
 
+// NAVBAR
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu1 = document.querySelector(".container-bottom-nav-bar");
+const navMenu2 = document.querySelector(".container-top-nav-bar");
 
 const detailsContainer= document.querySelector(".description-details");
 const details= document.querySelector(".block-product-description-details-text");
@@ -20,59 +22,110 @@ const materialsContainer=document.querySelector(".description-materials");
 const materials=document.querySelector(".block-product-description-materials-text");
 const materialsText=document.querySelector(".materials-text");
 
-// const sustainability=document.querySelector(".description-sustainability");
-
 const careContainer=document.querySelector(".description-care");
-const care=document.querySelector(".block-product-description-care-text")
-const careText=document.querySelector(".care-text")
+const care=document.querySelector(".block-product-description-care-text");
+const careText=document.querySelector(".care-text");
 
 const bodyListener= document.querySelector(".eventListner");
 
-const plus = document.querySelector(".vert-plus")
+const plusDetails = document.querySelector(".vert-plus-details");
+const plusFeatures = document.querySelector(".vert-plus-features");
+const plusTechnology = document.querySelector(".vert-plus-technology");
+const plusMaterials = document.querySelector(".vert-plus-materials");
+const plusCare = document.querySelector(".vert-plus-care");
 
 
-bodyListener.addEventListener("click", event => {
-    if(event.target=plus){
-        plus.classList.toggle("active");
-    }
 
-    if(event.target=detailsContainer){
-        details.classList.toggle("active");
-        detailsText.classList.toggle("active");
-    }
 
-    if(event.target=featuresContainer){
+function activateNav(){
+    hamburger.classList.toggle("active");
+    navMenu1.classList.toggle("active");       
+    navMenu2.classList.toggle("active");        
+ 
+}
+
+function activateDetails(){
+    details.classList.toggle("active");
+    detailsText.classList.toggle("active");
+    plusDetails.classList.toggle("active");
+}
+
+function activateFeatures(){
         features.classList.toggle("active");
-        featuresText.classList.toggle("active");
-    }
+        featuresText.classList.toggle("active");   
+        plusFeatures.classList.toggle("active");
+ 
+}
 
-    if(event.target=technologyContainer){
+function activateTechnology(){
         technology.classList.toggle("active");
         technologyText.classList.toggle("active");
-    }
+        plusTechnology.classList.toggle("active");
 
-    if(event.target=materialsContainer){
+
+}
+
+function activateMaterials(){
         materials.classList.toggle("active");
         materialsText.classList.toggle("active");
+        plusMaterials.classList.toggle("active");
 
-    }
-    // if(event.target=sustainability){
-    //     sustainability.classList.toggle("active");
-    // }
+}
 
-    if(event.target=careContainer){
+function activateCare(){
         care.classList.toggle("active");
         careText.classList.toggle("active");
-    }
+        plusCare.classList.toggle("active");
+
+}
+
+
+
+
+
+
+// bodyListener.addEventListener("click", event => {
+//     if(event.target=plus){
+//         plus.classList.toggle("active");
+//     }
+
+//     if(event.target=detailsContainer){
+//         details.classList.toggle("active");
+//         detailsText.classList.toggle("active");
+//     }
+
+//     if(event.target=featuresContainer){
+//         features.classList.toggle("active");
+//         featuresText.classList.toggle("active");
+//     }
+
+//     if(event.target=technologyContainer){
+//         technology.classList.toggle("active");
+//         technologyText.classList.toggle("active");
+//     }
+
+//     if(event.target=materialsContainer){
+//         materials.classList.toggle("active");
+//         materialsText.classList.toggle("active");
+
+//     }
+//     // if(event.target=sustainability){
+//     //     sustainability.classList.toggle("active");
+//     // }
+
+//     if(event.target=careContainer){
+//         care.classList.toggle("active");
+//         careText.classList.toggle("active");
+//     }
 
  
 
 
-    if(event.target=hamburger){
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");        
-    }
-})
+//     if(event.target=hamburger){
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");        
+//     }
+// })
 
 // details.addEventListener("click", () => {
 //     details.classList.toggle("active");
